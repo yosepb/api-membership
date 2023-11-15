3f9b71d831b7a2827ff9b419f988d43c347f44cb
yosepb <yosepbahtiar400@gmail.com>
2023-11-15 07:43:07 +0700

    Add routes.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
-
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Halo World");
-});
+const app = require("./routes");
 
 app.listen(process.env.PORT, () => {
   console.log(`Server sudah jalan di port ${process.env.PORT}.`);
